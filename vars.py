cef5c015effb8225bd1a88c0109696cbfd4390d6
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2024-03-24 19:47:39 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,5 +7,5 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 
-REMOVEBG_API = os.environ.get("REMOVEBG_API", "")
-UNSCREEN_API = os.environ.get("UNSCREEN_API", "")
+REMOVEBG_API = os.environ.get("REMOVEBG_API", "mUEGiJgeMHjVYNS848nzRXdh")
+UNSCREEN_API = os.environ.get("UNSCREEN_API", "m5dDivDgRhrxZmmem1X6ZZ21")
